5693a5c5f2d3a25211f977d75a1f7c1434eab61f
Jonathan Sebastian <jojosebseb@gmail.com>
2016-06-07 23:36:56 +0700

    added features

--- script/jquery.js
+++ script/jquery.js
@@ -2,3 +2,36 @@ $('.cartex-checkbox').on('click', function(){
    console.log("asd");
    $('.cartex-point').toggleClass('cartex-point-active')
 })
+
+
+$('.main-slick').slick({
+   infinite: true,
+   speed: 300,
+   slidesToShow: 1,
+   centerMode: true,
+   variableWidth: true,
+   arrows: false,
+   autoplay: true,
+   autoplaySpeed: 1000,
+});
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.shopdetail-thumb-div'
+});
+$('.shopdetail-thumb-div').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+});
+$(document).ready(function(){
+   $('.shopdetail-big-img').zoom();
+});
